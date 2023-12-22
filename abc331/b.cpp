d394afac71bfbe3aafbd71084a38d8160daa3fbc
kykougaku <6489550+kykougaku@users.noreply.github.com>
2023-12-22 14:13:00 +0900

    updated

--- abc331/b.cpp
+++ abc331/b.cpp
@@ -2,11 +2,24 @@
 
 using namespace std;
 
-int maiin(){
+int main(){
     int n,s,m,l;
     cin >> n >> s >> m >> l;
 
-    
+    int min = l*n;
+
+    for (int i=0; i<100; i++){
+        for(int j=0; j<100; j++){
+            for(int k=0; k<100; k++){
+                if(i*6+j*8+k*12 >= n){
+                    if(i*s + j*m + k*l <= min){
+                        min = i*s + j*m + k*l;
+                    }
+                }
+            }
+        }
+    }
+    cout << min << endl;
 
     return 0;
 }
---
+++ abc331/c.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for(int i=0; i<n; i++){
+        cin >> a.at(i);
+    }
+
+    for(int i=0; i<n; i++){
+        
+    }
+
+
+
+    return 0;
+}
