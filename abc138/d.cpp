11fb663a939a4947284b6b825bf0cd90a41e9cb5
kykougaku <6489550+kykougaku@users.noreply.github.com>
2024-01-18 11:08:14 +0900

    ac abc138/d.cpp

--- abc138/d.cpp
+++ abc138/d.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(vector<vector<int>> &g, vector<int> &ans, int oya, int v, int x){
-    ans[v] += x;
-    for(int nv : g[v]){
-        if(oya==nv) continue;
-        dfs(g, ans, v, nv, x);
+
+void dfs(vector<vector<int>> &g, int v, int p, vector<int> &ans){
+    for(auto nv : g[v]){
+        if(nv == p) continue;
+        ans[nv] += ans[v];
+        dfs(g, nv, v, ans);
     }
 }
 
@@ -13,25 +14,23 @@ int main(){
     int n, q;
     cin >> n >> q;
     vector<vector<int>> g(n);
-    vector<int> p(q), x(q);
-    vector<bool> seen(n, false);
+    vector<int> ans(n, 0);
     for(int i=0; i<n-1; i++){
         int a, b;
         cin >> a >> b;
         g[a-1].push_back(b-1);
+        g[b-1].push_back(a-1);
     }
     
-    for(int i=0; i<n-1; i++){
-
-    }
     for(int i=0; i<q; i++){
-        cin >> p[i] >> x[i];
+        int p,x;
+        cin >> p >> x;
+        p--;
+        ans[p] += x;
     }
 
-    vector<int> ans(n, 0);
-    for(int i=0; i<q; i++){
-        dfs(g, ans, p[i]-1, x[i]);
-    }
+    dfs(g, 0, -1, ans);
+
     
     for(int i=0; i<n; i++){
         cout << ans[i] << " ";
