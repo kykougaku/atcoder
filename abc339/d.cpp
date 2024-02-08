4b566791d6ac5e6c972c69a612462d3797784a14
kykougaku <6489550+kykougaku@users.noreply.github.com>
2024-02-08 14:21:56 +0900

    updated

--- abc339/d.cpp
+++ abc339/d.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
-int bfs(vector<vector<vector<vector<char>>>> map, vector<int> start, int n){
+
+int main (){
+    int n;
+    cin >> n;
+    vector <int> start;
+    vector<vector<char>> map(n, vector<char>(n));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> map.at(i).at(j);
+            if(map.at(i).at(j) == 'P'){
+                start.push_back(i);
+                start.push_back(j);
+            }
+        }
+    }
+
+    vector<vector<vector<vector<int>>>> dist(n, vector<vector<vector<int>>>(n, vector<vector<int>>(n, vector<int>(n, 10000000))));
+    
+    dist.at(start.at(0)).at(start.at(1)).at(start.at(2)).at(start.at(3)) = 0;
     queue<vector<int>> que;
     que.push(start);
 
@@ -11,40 +29,32 @@ int bfs(vector<vector<vector<vector<char>>>> map, vector<int> start, int n){
         que.pop();
         int x = pos.at(0);
         int y = pos.at(1);
+        int a = pos.at(2);
+        int b = pos.at(3);
+        if(x==a && y==b){
+            cout << dist.at(x).at(y).at(a).at(b) << endl;
+            return 0;
+        }
         for(int i = -1; i < 2; i++){
             for(int j = -1; j < 2; j++){
                 if(i == 0 || j == 0){
-                    if(x+i >= 0 && x+i < n && y+j >= 0 && y+j < n && map.at(x+i).at(y+j) != '#' && dist.at(x+i).at(y+j) == -1){
-                        dist.at(x+i).at(y+j) = dist.at(x).at(y) + 1;
-                        que.push({x+i, y+j});
-                        if(map.at(x+i).at(y+j) == 'P'){
-                            return dist.at(x+i).at(y+j);
-                        }
+                    if(x+i >= 0 && x+i < n && y+j >= 0 && y+j < n && map.at(x+i).at(y+j) != '#' && a+i >= 0 && a+i < n && b+j >= 0 && b+j < n && map.at(a+i).at(b+j) != '#' && dist.at(x+i).at(y+j).at(a+i).at(b+j) == 10000000){   
+                        dist.at(x+i).at(y+j).at(a+i).at(b+j) = dist.at(x).at(y).at(a).at(b) + 1;
+                        que.push({x+i, y+j, a+i, b+j});
+                    }
+                    else if(x+i >= 0 && x+i < n && y+j >= 0 && y+j < n && map.at(x+i).at(y+j) != '#' && dist.at(x+i).at(y+j).at(a).at(b) == 10000000){
+                        dist.at(x+i).at(y+j).at(a).at(b) = dist.at(x).at(y).at(a).at(b) + 1;
+                        que.push({x+i, y+j, a, b});
+                    }
+                    else if(a+i >= 0 && a+i < n && b+j >= 0 && b+j < n && map.at(a+i).at(b+j) != '#' && dist.at(x).at(y).at(a+i).at(b+j) == 10000000){
+                        dist.at(x).at(y).at(a+i).at(b+j) = dist.at(x).at(y).at(a).at(b) + 1;
+                        que.push({x, y, a+i, b+j});
                     }
                 }
             }
         }
     }
 
-    return -1;
-}
-
-int main (){
-    int n;
-    cin >> n;
-    vector <int> start(2);
-    vector<vector<vector<vector<char>>>> map(n, vector<char>(n));
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> map.at(i).at(j);
-            if(map.at(i).at(j) == 'P'){
-                start.at(0) = i;
-                start.at(1) = j;
-            }
-        }
-    }
-    int ans = bfs(map, start, n);
-
-    cout << ans << endl;
+    cout << -1 << endl;
     return 0;
 }
