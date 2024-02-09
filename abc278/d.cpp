4f90e5e162ae68b203fdd6b005c82e600a88d7f9
kykougaku <6489550+kykougaku@users.noreply.github.com>
2024-02-09 13:23:41 +0900

    updated with abc278

--- abc278/d.cpp
+++ abc278/d.cpp
@@ -1,25 +1,36 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
 int main(){
-    int N;
+    int64_t N;
     cin >> N;
-    vector<int> A(N);
-    for(int i = 0; i < N; i++) cin >> A[i];
-    int Q;
+    vector<int64_t> A(N);
+    for(int64_t i = 0; i < N; i++) cin >> A[i];
+    int64_t Q;
     cin >> Q;
-    vector<int> B(Q), C(Q), D(Q);
-    for(int i = 0; i < Q; i++){
+    vector<int64_t> B(Q), C(Q), D(Q);
+    for(int64_t i = 0; i < Q; i++){
         cin >> B[i] >> C[i];
         if(B[i] == 2) cin >> D[i];
     }
 
-    int s = 0;
-    for(int i = 0; i < Q; i++){
+    stack <int64_t> added_index;
+    for(int64_t i = 0; i < N; i++) added_index.push(i);
+
+    int64_t s = 0;
+    for(int64_t i = 0; i < Q; i++){
         if(B[i] == 1){
             s = C[i];
+            while(!added_index.empty()){
+                A.at(added_index.top()) = 0;
+                added_index.pop();
+            }
+            
         }
         else if(B[i] == 2){
             A.at(C[i]-1) += D[i];
+            added_index.push(C[i]-1);
         }
         else if(B[i] == 3){
             cout << A.at(C[i]-1) + s << endl;
