ed05ccf2a62ea680b9dbfd702237b85eb911f227
kykougaku <6489550+kykougaku@users.noreply.github.com>
2024-02-17 10:58:48 +0900

    updated 337d

--- abc337/d.cpp
+++ abc337/d.cpp
@@ -17,46 +17,62 @@ int main (){
     for(int i = 0; i < h; i++){
         int counto = 0;
         int countdot = 0;
+        int block =-1;
         for(int j = 0; j < w; j++){
             if(v.at(i).at(j) == 'x'){
                 counto = 0;
                 countdot = 0;
+                block = j;
             }
             else if(v.at(i).at(j) == 'o'){
                 counto++;
-                counto = min(counto, k);
             }
             else if(v.at(i).at(j) == '.'){
                 countdot++;
-                countdot = min(countdot, k);
             }
 
-            if(counto+countdot >= k){
-                ans = min(ans, k-counto);
+            if(j-block < k) continue;
+
+            if(j-k>=0 and v.at(i).at(j-k) == 'o'){
+                counto--;
             }
+            else if(j-k>=0 and v.at(i).at(j-k) == '.'){
+                countdot--;
+            }
+
+            ans = min(ans, countdot);
         }
     }
 
     for(int i = 0; i < w; i++){
         int counto = 0;
         int countdot = 0;
+        int block = -1;
         for(int j = 0; j < h; j++){
             if(v.at(j).at(i) == 'x'){
                 counto = 0;
                 countdot = 0;
+                block = j;
             }
             else if(v.at(j).at(i) == 'o'){
                 counto++;
-                counto = min(counto, k);
             }
             else if(v.at(j).at(i) == '.'){
                 countdot++;
-                countdot = min(countdot, k);
             }
 
-            if(counto+countdot >= k){
-                ans = min(ans, k-counto);
+            if(j-block < k) continue;
+
+
+            if(j-k>=0 and v.at(j-k).at(i) == 'o'){
+                counto--;
             }
+            else if(j-k>=0 and v.at(j-k).at(i) == '.'){
+                countdot--;
+            }
+
+            ans = min(ans, countdot);
+
         }
     }
 
