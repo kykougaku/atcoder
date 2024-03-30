35d898544ea09562ece0730b73fd03c632aa72aa
kykougaku <6489550+kykougaku@users.noreply.github.com>
2024-03-30 20:48:34 +0900

    updated

--- abc344/d.cpp
+++ abc344/d.cpp
@@ -1,41 +1,40 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-const int INF = 1e9;
+const int INF = 1<<30;
 
 int main (){
-    string t;
-    cin >> t;
-    int n;
-    cin >> n;
-    vector<vector<string>> s (n, vector<string>());
+    string t; int n;
+    cin >> t >> n;
     vector<int> a(n);
+    vector<vector<string>> ss(n, vector<string>());
     for(int i = 0; i < n; i++){
-        cin >> a.at(i); 
+        cin >> a.at(i);
         for(int j = 0; j < a.at(i); j++){
             string temp;
             cin >> temp;
-            s.at(i).push_back(temp);
+            ss.at(i).push_back(temp);
         }
     }
-    vector<vector<int>> dp(101, vector<int>(101, INF));
 
+    vector<vector<int>> dp(n+1, vector<int>(t.size()+1, INF));
     dp.at(0).at(0) = 0;
+    int sssize;
+    for(int i = 1; i<=n; i++){
+        for(int j = 0; j<=t.size(); j++){
+            dp.at(i).at(j) = min(dp.at(i).at(j), dp.at(i-1).at(j));
 
-    for(int j = 1; j<n; j++){
-        for(int i = 0; i<=t.size(); i++){
-            dp.at(i).at(j) = min(dp.at(i).at(j), dp.at(i).at(j-1));
-        }
-
-        for(int i = 0; i<=t.size(); i++){
-            for(auto si: s.at(j)){
-                if(t.substr(i, si.size()) == si){
-                    dp.at(i+si.size()).at(j) = min(dp.at(i+si.size()).at(j), dp.at(i).at(j-1) + 1);
+            for(int k = 0; k<a.at(i-1); k++){
+                //袋iまで使い末尾がss.at(i).at(k)であるようなSを作りTのj文字目まで一致させる
+                sssize = ss.at(i-1).at(k).size();
+                if(j-sssize < 0)continue;
+                if(t.substr(j-sssize,sssize) == ss.at(i-1).at(k) and dp.at(i-1).at(j-sssize) < INF){
+                    dp.at(i).at(j) = min(dp.at(i).at(j), dp.at(i-1).at(j-sssize)+1);
                 }
             }
         }
     }
-    cout << dp.at(t.size()).at(n-1) << endl;
-
+    if(dp.at(n).at(t.size()) >= INF) cout << -1 << endl;
+    else  cout << dp.at(n).at(t.size()) << endl;
     return 0;
 }
